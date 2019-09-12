e07b52357da9388e77fd3183b9abb5388af70c14
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-12 17:07:55 -0500

    initial setup with dependencies, create store, provide store, initial state

---
+++ beer-app/src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+    beer: {
+        name:'',
+        abv:'',
+        shortName: '',
+        description:'',
+        og: '',
+        fg: ''
+    },
+
+    isFetching: null,
+    error:''
+};
+export const reducer = (state = initialState, action) => {
+
+} 
